35eab8abaab6fc37e071fe44f7dbbd27ac253943
Jerry Liu <jerrylzy@ucla.edu>
2016-10-22 01:16:59 -0700

    Time Stamp Conversion, CSS Update, Added Refresh Button 	* main.js: Declared a function called 'getData' which is an anonymous function before. Added a function called 'refresh' when the mouse clicks on the refresh button. Changed the source from last week to past day. 	* index.html: Changes incoporate with the custom CSS file. 	* style.css: Brought in the CSS we used last time.

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-// console.log("hi");
-//
-// $(document).ready(function(){
-//   $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson", function(data)
-//   {
-//     var source   = $("#entry-template").html();
-//     var template = Handlebars.compile(source);
-//
-//     console.log(data);
-//     alert( "Load was performed." );
-//   });
-// });
-
-
-$(document).ready(function(){
-  $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson", function(data){
-    console.log(data);
+function getData() {
+    $.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson", function(data) {
+      console.log("Called.");
 
       var source   = $("#entry-template").html();
       var template = Handlebars.compile(source);
@@ -22,4 +8,14 @@ $(document).ready(function(){
 
       $("#data").html(html);
   });
+}
+
+$(document).ready(getData());
+
+Handlebars.registerHelper("Convert", function(timeStamp) {
+	return new Date(timeStamp);
 });
+
+function refresh() {
+    getData();
+}
